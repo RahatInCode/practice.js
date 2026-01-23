a67e4480cc1c5093834009d4a8bb67e6dc1ea05b
RahatinCode <ahmedrahat9901@gmail.com>
2026-01-23 21:06:52 +0600

    learning conditionals

---
+++ conditionals/if-else.js
@@ -0,0 +1,12 @@
+/**
+ * Simple Login Validation
+ */
+
+let username = "admin";
+let password = "1234";
+
+if (username === "admin" && password === "1234") {
+  console.log("Login successful");
+} else {
+  console.log("Invalid credentials");
+}
