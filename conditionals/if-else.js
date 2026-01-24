fc5fa02539b9e3c452790eae7a3be3466443289f
RahatinCode <ahmedrahat9901@gmail.com>
2026-01-24 23:29:05 +0600

    ? updating the values

--- conditionals/if-else.js
+++ conditionals/if-else.js
@@ -3,10 +3,11 @@
  */
 
 let username = "admin";
-let password = "1234";
+let password = "12345651";
 
 if (username === "admin" && password === "1234") {
   console.log("Login successful");
 } else {
   console.log("Invalid credentials");
 }
+
